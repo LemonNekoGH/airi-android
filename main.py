035eff3d5c62539891d69bbc12db6dbe422b3b70
LemonNeko <chheese048@gmail.com>
2025-04-19 01:58:50 +0800

    feat: connect, tap screen, devices

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from mcp.server.fastmcp import FastMCP
 
+import ppadb.client as client
+
+from ppadb.device import Device
+
+adb_client = client.Client()
+device: Device | None = None
+
 # Create an MCP server
-mcp = FastMCP("Demo")
+mcp = FastMCP("airi-android")
 
 
-# Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def connect_device(host: str, port: int):
+    """Connect to the device"""
+    global device
+    device = adb_client.connect(f"{host}:{port}")
 
 
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
+@mcp.tool()
+def tap_screen(x: int, y: int):
+    """Tap the screen at the given coordinates"""
+    device.shell(f"input tap {x} {y}")
+
+
+@mcp.resource("adb://devices")
+def devices():
+    """Get all connected devices"""
+    return adb_client.devices()
