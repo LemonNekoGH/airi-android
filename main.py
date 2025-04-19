f56c7f8fdf18ddc105dede0e225d620fbe783f3a
LemonNeko <chheese048@gmail.com>
2025-04-20 00:23:49 +0800

    fix: use description instead of document str

--- main.py
+++ main.py
@@ -11,20 +11,34 @@ device: Device | None = None
 mcp = FastMCP("airi-android")
 
 
-@mcp.tool()
+@mcp.tool(description="Connect to the device")
 def connect_device(host: str, port: int):
     """Connect to the device"""
     global device
     device = adb_client.connect(f"{host}:{port}")
 
 
-@mcp.tool()
-def tap_screen(x: int, y: int):
-    """Tap the screen at the given coordinates"""
-    device.shell(f"input tap {x} {y}")
+@mcp.tool(description="Tap on the screen at the given coordinates")
+def input_tap(x: int, y: int):
+    device.input_tap(x, y)
 
 
-@mcp.resource("adb://devices")
+@mcp.tool(description="Select the device to use, you can get the list of devices with `devices` command")
+def select_device(serial: str):
+    global device
+    device = adb_client.device(serial)
+
+
+@mcp.tool(description="Run a shell command")
+def shell(command: str):
+    device.shell(command)
+
+
+@mcp.tool(description="List all connected ADB devices")
 def devices():
-    """Get all connected devices"""
-    return adb_client.devices()
+    devices_list: list[str] = []
+
+    for device in adb_client.devices():
+        devices_list.append(device.serial)
+
+    return devices_list
