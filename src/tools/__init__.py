3a512a0429f2a21b1ac6b918fd9c11450f5e7ee8
LemonNeko <chheese048@gmail.com>
2025-04-21 22:43:05 +0800

    feat: support more tools

---
+++ src/tools/__init__.py
@@ -0,0 +1,15 @@
+from .device import register_device_tools
+from .input import register_input_tools
+from .utils import register_utils_tools
+from .cpustat import register_cpustat_tools
+from .batterystats import register_batterystats_tools
+from .wm import register_wm_tools
+
+__all__ = [
+    "register_device_tools",
+    "register_input_tools",
+    "register_utils_tools",
+    "register_cpustat_tools",
+    "register_batterystats_tools",
+    "register_wm_tools",
+]
